2a217fd854d3cb2bd33de71de6a64c0de550b8a7
contribute1986 <llb1127@163.com>
2018-10-31 18:04:08 +0800

    addnewfile

---
+++ seajsLearn/2.js
@@ -0,0 +1,8 @@
+define(function(require,exports,module){
+    module.exports={
+      foo: 'bar',
+      doSomething: function() {
+        alert('2.js doSomething 函数调用');
+      }
+    }
+})
---
+++ 领悟/XAMPP问题解决清单.md
@@ -0,0 +1,2 @@
+#1无法启动Apache
+https://zhidao.baidu.com/question/1860648670835110227.html
